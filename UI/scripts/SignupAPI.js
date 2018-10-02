9d8c401bbefbcea3451c57d4174c9d576aa58b62
alovega <alovegakevin@gmail.com>
2018-10-02 13:20:56 +0300

    updates my API to be able to catch an error

--- UI/scripts/SignupAPI.js
+++ UI/scripts/SignupAPI.js
@@ -1,18 +1,30 @@
-document.getElementById('submit').addEventListener('click', user);
-
+window.addEventListener("load", powerOn);
+function powerOn(){
+    document.getElementById('submit').addEventListener("click", user);
+}
 function user(){
     let email = document.getElementById('email').value;
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
-    fetch('http://127.0.0.1:5000/api/v1/auth/signup',{
+    fetch('http://127.0.0.1:5000/auth/signup',{
         method:'POST',
         headers:{
-         'Accept':'application/json, text/plain, */*',
+         'Accept':'application/json',
          'Content-type': 'application/json;charset=UTF-8'  
         },
         body:JSON.stringify({email:email, username:username, password:password})
     })
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error))
+    .then(function(response){
+        if(!response.ok){
+            throw Error(response.status.text);
+        }
+        return response.json();
+    })
+    
+    .then(function (data){
+        console.log(data)})
+
+    .catch(function(error){
+        console.log('Looks like there was a problem:\n',error)
+    })
 }
